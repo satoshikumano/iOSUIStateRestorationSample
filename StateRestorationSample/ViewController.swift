a4f55f288d01452e3394ebb46fd8bd04921b8c7e
satoshi <satoshi.kumano@kii.com>
2015-08-04 19:19:06 +0900

    Added preserve/ restore functionality.

--- StateRestorationSample/ViewController.swift
+++ StateRestorationSample/ViewController.swift
@@ -12,8 +12,8 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var myText: UITextField!
     override func viewDidLoad() {
+        println("viewDidLoad")
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,6 +21,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func encodeRestorableStateWithCoder(coder: NSCoder) {
+        println("encodeRestorableStateWithCoder")
+        super.encodeRestorableStateWithCoder(coder)
+        coder.encodeObject(myText.text, forKey: "mytext")
+    }
+
+    override func decodeRestorableStateWithCoder(coder: NSCoder) {
+        println("decodeRestorableStateWithCoder")
+        super.decodeRestorableStateWithCoder(coder)
+        myText.text = coder.decodeObjectForKey("mytext") as! String
+    }
 
 }
 
