b29e08790c80cb7211ef97fb32742f018eb3672c
satoshi <satoshi.kumano@kii.com>
2015-08-04 15:58:03 +0900

    Initial Commit

---
+++ StateRestorationSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StateRestorationSample
+//
+//  Created by 熊野 聡 on 2015/08/04.
+//  Copyright (c) 2015年 Kii. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
